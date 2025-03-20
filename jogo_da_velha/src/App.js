d3b04fb14e7817f106f0594b084426207e233251
Pedro Henrique Gregório <pedrosgregorio@gmail.com>
2025-03-20 18:56:23 -0300

    Refatorando

--- jogo_da_velha/src/App.js
+++ jogo_da_velha/src/App.js
@@ -175,47 +175,6 @@ function Tabuleiro({ tabuleiro, setTabuleiro, vezX, setVezX }) {
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    }
-    onPlay(nextSquares);
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
-  return (
-    <>
-      <div className="board-row">
-        <Quadrado value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Quadrado value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Quadrado value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Quadrado value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Quadrado value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Quadrado value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Quadrado value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Quadrado value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Quadrado value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
-}
-
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
